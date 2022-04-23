09ae08a58d3dc825a97610c7eb6e01fc7957793c
jtamara1 <jeantamara@hotmail.com>
2022-04-23 08:51:26 -0500

    inicializado repositorio con contenidos de repositorio fundamentos_react_bp

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import Formulario from './components/Formulario'
+
+function App() {
+  return (
+    <>
+      
+        
+
+        <Formulario/>
+      
+
+    </>
+  );
+}
+
+export default App;
